634c9a355309ac6f87e781d33f904d75c3307abf
Thiago Leão Freire <thiagofreire@digix.com.br>
2022-09-14 22:33:38 -0300

    implementando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("TELEFONE");
+var nokia = new Nokia("1010", "K", "353535", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+var iphone = new Iphone("3030", "C", "505050", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
